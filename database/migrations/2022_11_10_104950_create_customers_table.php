ec64b726fbb2b65e56ec3d9374f69d1d0c90d430
Hamza Imran <81363294+Hamza-Imran435@users.noreply.github.com>
2022-11-15 15:29:56 +0500

    code added

---
+++ database/migrations/2022_11_10_104950_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age');
+            $table->string('country');
+            $table->string('private_image');
+            $table->string('private_videos');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
